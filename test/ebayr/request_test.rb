70bcd4d9fb0d358e1ea9a2549655d58dba8fa9e7
SpeerJ <Johann.speer@gmail.com>
2015-04-08 00:45:52 -0500

    update tests to reflect changes
    Sorry, i added the last commits on so late, this is my first real contribution to repos other than my own. I'm a bit of a noob.

--- test/ebayr/request_test.rb
+++ test/ebayr/request_test.rb
@@ -3,16 +3,30 @@ require 'test_helper'
 require 'ebayr/request'
 
 describe Ebayr::Request do
+
   describe "serializing input" do
     it "converts times" do
-      result = Ebayr::Request.serialize_input("Time" => Time.utc(2010, 'oct', 31, 03, 15))
-      result['Time'].must_equal "2010-10-31T03:15:00Z"
+      result = Ebayr::Request.serialize_input(Time.utc(2010, 'oct', 31, 03, 15))
+      result.must_equal "2010-10-31T03:15:00Z"
     end
   end
 
   describe "uri" do
     it "is the Ebayr one" do
       Ebayr::Request.new(:Blah).uri.must_equal(Ebayr.uri)
+    end
+  end
+
+  describe "arrays" do
+    it "converts multiple arguments in new function" do
+      args = [{ :a => 1 }, { :a => [{:b => 1 }, { :b => 2 }] }]
+      Ebayr::Request.new(:Blah, input: args).input_xml.must_equal '<a>1</a><a><b>1</b><b>2</b></a>'
+    end
+
+    it "converts times" do
+      args = [{ :Time => Time.utc(2010, 'oct', 31, 03, 15)}]
+      result = Ebayr::Request.new(:Blah, args).input_xml
+      result.must_equal "<Time>2010-10-31T03:15:00Z</Time>"
     end
   end
 
